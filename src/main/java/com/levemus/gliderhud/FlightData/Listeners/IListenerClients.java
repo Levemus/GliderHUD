d6bfbb7dd91a7c8cea7745ba720f597ccae8f7be
Levemus <mark@levemus.com>
2015-12-27 09:22:45 -0800

    Adding.

---
+++ src/main/java/com/levemus/gliderhud/FlightData/Listeners/IListenerClients.java
@@ -0,0 +1,23 @@
+package com.levemus.gliderhud.FlightData.Listeners;
+
+/*
+ Both the author and publisher makes no representations or warranties
+ about the suitability of this software, either expressed or implied, including
+ but not limited to the implied warranties of merchantability, fitness
+ for a particular purpose or noninfringement. Both the author and publisher
+ shall not be liable for any damages suffered as a result of using,
+ modifying or distributing the software or its derivatives.
+
+ (c) 2015 Levemus Software, Inc.
+ */
+
+import com.levemus.gliderhud.FlightData.IFlightDataClient;
+
+import java.util.HashSet;
+
+/**
+ * Created by markcarter on 15-12-26.
+ */
+public interface IListenerClients {
+    public HashSet<IFlightDataClient> clients();
+}
---
+++ src/main/java/com/levemus/gliderhud/FlightData/Listeners/IListenerData.java
@@ -0,0 +1,24 @@
+package com.levemus.gliderhud.FlightData.Listeners;
+
+/*
+ Both the author and publisher makes no representations or warranties
+ about the suitability of this software, either expressed or implied, including
+ but not limited to the implied warranties of merchantability, fitness
+ for a particular purpose or noninfringement. Both the author and publisher
+ shall not be liable for any damages suffered as a result of using,
+ modifying or distributing the software or its derivatives.
+
+ (c) 2015 Levemus Software, Inc.
+ */
+
+import com.levemus.gliderhud.FlightData.IFlightData;
+
+import java.util.HashSet;
+import java.util.UUID;
+
+/**
+ * Created by markcarter on 15-12-26.
+ */
+public interface IListenerData {
+    void onData(HashSet<UUID> channels, IFlightData data);
+}
